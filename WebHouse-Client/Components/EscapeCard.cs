be9ac9bb3e48afd659526333f46579806276a9fc
JorgeLife202 <joschua.kux@icloud.com>
2025-05-08 11:07:27 +0200

    Splash Background added Numbers removed for one numer in center room and numer white #28 Fluchtkarten

--- WebHouse-Client/Components/EscapeCard.cs
+++ WebHouse-Client/Components/EscapeCard.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing.Drawing2D;
+using System.Reflection;
 using WebHouse_Client.Logic;
 
 namespace WebHouse_Client.Components;
@@ -8,12 +9,12 @@ public class EscapeCard
     private Card CardComponent { get; }
     public Panel Panel => CardComponent.Panel;
     public Logic.EscapeCard Card { get; }
-
     public EscapeCard(Logic.EscapeCard card)
     {
         Card = card;
         
-        CardComponent = new Card(new Size(135, 200), 5, 10, Card.Color.GetColor(), 2);
+        CardComponent = new Card(new Size(135, 200), 5, 10,
+            Color.Black, 2);
         Panel.Paint += DrawEscapeCards;
     }
     private void DrawEscapeCards(object? sender, PaintEventArgs e)
@@ -21,29 +22,35 @@ public class EscapeCard
         Graphics g = e.Graphics;
         g.SmoothingMode = SmoothingMode.AntiAlias;
         
+        SplashBackground(g);
         DrawNumber(g);
         DrawRoom(g);
     }
-    private void DrawNumber(Graphics g) //Zeichnet die Nummern oben, links und rechts
+    private void SplashBackground(Graphics g) //Splash Hintergrung
     {
-        using (Font font = new Font("Arial", 10, FontStyle.Bold))
-        using (var brush = new SolidBrush(Color.Black))
+        Image SplashBackgroundImage = Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("WebHouse_Client.Resources.Background_Images.SplashBackground.png"));
+        
+        var rect = new Rectangle(Panel.ClientRectangle.Width / 20, Panel.ClientRectangle.Width / 20, 
+            Panel.ClientRectangle.Width - Panel.ClientRectangle.Width / 20 * 2, 
+            Panel.ClientRectangle.Height - Panel.ClientRectangle.Width / 20 * 2);
+        g.FillRectangle(new SolidBrush(Card.Color.GetColor()), rect);
+        g.DrawImage(SplashBackgroundImage, rect);
+    }
+    private void DrawNumber(Graphics g) //Zeichnet die Nummer unter dem Raumnamen
+    {
+        using (Font font = new Font("Arial", 15, FontStyle.Bold))
+        using (var brush = new SolidBrush(Color.White))
         {
             string text = Card.Number.ToString();
-            SizeF size = g.MeasureString(text, font);
+            SizeF numberSize = g.MeasureString(text, font);
             
-            // Nummer links oben
-            var leftPos = new PointF(10, 10);
-            g.DrawString(text, font, brush, leftPos);
-
-            // Nummer rechts oben
-            var rightPos = new PointF(Panel.Width - size.Width - 10, 10);
-            g.DrawString(text, font, brush, rightPos);
+            var centerPos = new PointF((Panel.Width - numberSize.Width)/2, ((Panel.Height - numberSize.Height) / 2)+ 25);
+            g.DrawString(text, font, brush, centerPos);
         }
     }
     private void DrawRoom(Graphics g) // Zeichnet den Raumnamen in der Kartenmitte
     {
-        using (Font font = new Font("Arial", 10, FontStyle.Bold))
+        using (Font font = new Font("Arial", 14, FontStyle.Bold))
         using (var brush = new SolidBrush(Color.Black))
         {
             SizeF roomSize = g.MeasureString(Card.Room, font);
