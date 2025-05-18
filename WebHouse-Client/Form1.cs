188b8a52a0a8026ea153212f2af47c2ce305eceb
DuBis <DuBis@LENNOXLAPTOP>
2025-05-18 17:25:23 +0200

    Lobby Layout relativ zur größe des Fensters (und Knopf).

--- WebHouse-Client/Form1.cs
+++ WebHouse-Client/Form1.cs
@@ -7,18 +7,11 @@ public partial class Form1 : Form
 {
     public Form1()
     {
-        BackgroundImage = Image.FromFile("C:\\Users\\DuBis\\RiderProjects\\WebHouseLennox\\WebHouse-Client\\Resources\\Background_Images\\LoginBackground.jpeg");
-        //string path = Path.Combine(Application.StartupPath, "Resources", "Background_Images", "LoginBackground.jpeg");
-        //Image image = Image.FromStream(
-            //Assembly.GetExecutingAssembly().GetManifestResourceStream("WebHouse_Client.Resources.Background_Images.LoginBackground.jpeg"));
-        //string path = Path.Combine(Application.StartupPath, "Resources", "Background_Images", "LoginBackground.jpeg");
-        //this.FormBorderStyle = FormBorderStyle.None; //kein Rand
-        //this.WindowState = FormWindowState.Maximized; //macht Vollbild
-        //Assembly.GetExecutingAssembly().GetManifestResourceStream
-            //("WebHouse_Client.Resources.Background_Images.LoginBackground.jpeg");
         InitializeComponent();
-        this.Width = 1920;
-        this.Height = 1080;
+        BackgroundImage = Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("WebHouse_Client.Resources.Background_Images.LoginBackground.jpeg"));
+        this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        this.MaximizeBox = false;
+        this.ClientSize = new Size(1920, 1080);
     }
 
     private void GameFormBTN_Click(object sender, EventArgs e)
--- WebHouse-Client/Lobby.cs
+++ WebHouse-Client/Lobby.cs
@@ -1,19 +1,47 @@
+using WebHouse_Client.Components;
+using System.Reflection;
+using System.Drawing;
+
 namespace WebHouse_Client;
 
 public partial class Lobby : Form
 {
+    private float aspectRatio;
+
     public Lobby()
     {
         InitializeComponent();
-        BackgroundImage = Image.FromFile("C:\\Users\\DuBis\\RiderProjects\\WebHouseLennox\\WebHouse-Client\\Resources\\Background_Images\\Lobby.jpg");
-        this.Width = 1920;
+        BackgroundImage = Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("WebHouse_Client.Resources.Background_Images.LobbyFertig.png")); //BackgroundImage
+        this.BackgroundImageLayout = ImageLayout.Stretch;
+        this.Width = 1920; //Startgröße
         this.Height = 1080;
+        this.MaximizeBox = false;
+        aspectRatio = (float)this.Width / this.Height; //Seitenverhältnis berechnen
+        this.ResizeEnd += Lobby_ResizeEnd; //Größe ändern Event
+        UpdateStartButtonLayout();
+    }
+
+    private void Lobby_ResizeEnd(object sender, EventArgs e)
+    {
+        // Nur Höhe anpassen, damit das Seitenverhältnis bleibt
+        int newHeight = (int)(this.Width / aspectRatio);
+        this.Size = new Size(this.Width, newHeight);
+        UpdateStartButtonLayout();
+    }
+
+    private void UpdateStartButtonLayout()
+    {
+        Startbtn.Size = new Size(this.ClientSize.Width / 5, this.ClientSize.Height / 12); //proportionale Größe
+        //Button-Position zentriert, 80% Höhe
+        int x = (this.ClientSize.Width - Startbtn.Width) / 2; //mittig
+        int y = (int)(this.ClientSize.Height * 0.8);
+        Startbtn.Location = new Point(x, y);
     }
 
     private void button1_Click(object sender, EventArgs e)
     {
         GameForm gameForm = new GameForm();
-        gameForm.Show();  //Zeige die neue Form
-        this.Hide(); //Verstecke die aktuelle Form
+        gameForm.Show();
+        this.Hide();
     }
 }
