9fcafdfdca5a60d9d2cf3ea024c3d9d476ab8879
Peasplayer <peasplayer@peasplayer.xyz>
2025-05-19 18:29:34 +0200

    Added Lobby and host mechanics

---
+++ WebHouse-Client/Networking/Packets/SyncLobbyPacket.cs
@@ -0,0 +1,11 @@
+﻿namespace WebHouse_Client.Networking.Packets;
+
+public class SyncLobbyPacket
+{
+    public List<Player> Players;
+
+    public SyncLobbyPacket(List<Player> players)
+    {
+        Players = players;
+    }
+}
