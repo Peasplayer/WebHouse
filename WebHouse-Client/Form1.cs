f0e8048b59baa6bf7f98d84c2bdb00fd4519dc6a
Jannis Edler <jannisedler4@gmail.com>
2025-05-19 19:33:43 +0200

    p

--- WebHouse-Client/Form1.cs
+++ WebHouse-Client/Form1.cs
@@ -7,13 +7,6 @@ public partial class Form1 : Form
 {
     public Form1()
     {
-        
-        var form = new GameForm();
-        form.Show();
-        this.Hide();
-        return;
-        
-        
         InitializeComponent();
         this.DoubleBuffered = true;
         BackgroundImage = Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("WebHouse_Client.Resources.Background_Images.LogIn.png"));
