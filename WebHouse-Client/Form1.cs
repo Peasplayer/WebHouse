86c7d2012a49348ff3c89c0d2ad5cc42a7340b0c
Jannis Edler <jannisedler4@gmail.com>
2025-05-19 14:21:52 +0200

    Größe des Namens und IP eingabe Feldes geändert

--- WebHouse-Client/Form1.cs
+++ WebHouse-Client/Form1.cs
@@ -17,20 +17,24 @@ public partial class Form1 : Form
         this.MinimizeBox = false;
         this.StartPosition = FormStartPosition.CenterScreen;
         
-        Startbtn.Size = new Size(500 * Width / 1920, 125 * Height / 1080);
-        Startbtn.Location = new Point((Width - Startbtn.Size.Width) / 2, 800 * Height / 1080);
+        Startbtn.Size = new Size(500 * ClientSize.Width / 1920, 125 * ClientSize.Height / 1080);
+        Startbtn.Location = new Point((ClientSize.Width - Startbtn.Size.Width) / 2, 800 * ClientSize.Height / 1080);
         Startbtn.BackgroundImage = Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("WebHouse_Client.Resources.Background_Images.Start.png"));
 
-        textBox1.Size = new Size(500 * Width / 1920, 125 * Height / 1080);
-        textBox1.Location = new Point(900 * Width / 1920, 180 * Height / 1080);
+        textBox1.Size = new Size(500 * ClientSize.Width / 1920, 125 * ClientSize.Height / 1080); //Höhe wird duch den Font überschrieben
+        textBox1.Location = new Point(900 * ClientSize.Width / 1920, 194 * ClientSize.Height / 1080);
+        textBox1.Font = new System.Drawing.Font("Segoe UI", 42F * ClientSize.Height / 1080, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+
         
-        textBox2.Size = new Size(500 * Width / 1920, 125 * Height / 1080);
-        textBox2.Location = new Point(900 * Width / 1920, 325 * Height / 1080);
+        textBox2.Size = new Size(500 * ClientSize.Width / 1920, 125 * ClientSize.Height / 1080); //Höhe wird duch den Font überschrieben
+        textBox2.Location = new Point(900 * ClientSize.Width / 1920, 350 * ClientSize.Height / 1080);
+        textBox2.Font = new System.Drawing.Font("Segoe UI", 41F * ClientSize.Height / 1080, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+
     }
 
     private void Startbtn_Click(object sender, EventArgs e)
     {
-        if (textBox1.Text == "" || textBox2.Text == "")
+        if (textBox1.Text == "" || textBox2.Text == "") 
         {
             MessageBox.Show("Bitte gebe eine IP und einen Namen an!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
             return;
